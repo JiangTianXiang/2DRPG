1f27cad7c7f359590e732801fc97091f28f8ed34
lin <lin@v1022-rn-91-200.campus-dynamic.uwaterloo.ca>
2018-01-17 22:48:18 -0500

    Basic functions in the description

---
+++ MTE100/Assets/Maps/tilesets/roguelikeSheet_transparent.tsx
@@ -0,0 +1,119 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="roguelikeSheet_transparent" tilewidth="16" tileheight="16" spacing="1" tilecount="1767">
+ <image source="../../../../../Resources/roguelike-pack/Spritesheet/roguelikeSheet_transparent.png" width="968" height="526"/>
+ <terraintypes>
+  <terrain name="Water" tile="60"/>
+  <terrain name="Dirt" tile="578"/>
+ </terraintypes>
+ <tile id="2" terrain=",,,0">
+  <objectgroup draworder="index">
+   <object id="0" x="4" y="4" width="12" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="3" terrain=",,0,0">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="4" width="16" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="4" terrain=",,0,">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="4" width="12" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="57" terrain="0,0,0,">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0">
+    <polygon points="0,0 0,16 10,16 16,10 16,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="58" terrain="0,0,,0">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0">
+    <polygon points="0,0 0,12 4,16 16,16 16,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="59" terrain=",0,,0">
+  <objectgroup draworder="index">
+   <object id="0" x="4" y="0" width="12" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="60" terrain="0,0,0,0"/>
+ <tile id="61" terrain="0,,0,">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="12" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="114" terrain="0,,0,0">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0">
+    <polygon points="0,0 12,0 16,4 16,16 0,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="115" terrain=",0,0,0">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="4">
+    <polygon points="0,0 4,-4 16,-4 16,12 0,12"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="116" terrain=",0,,">
+  <objectgroup draworder="index">
+   <object id="0" x="4" y="0" width="12" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="117" terrain="0,0,,">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="118" terrain="0,,,">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="12" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="518" terrain="1,1,1,"/>
+ <tile id="519" terrain="1,1,,1"/>
+ <tile id="520" terrain=",,,1"/>
+ <tile id="521" terrain=",,1,1"/>
+ <tile id="522" terrain=",,1,"/>
+ <tile id="575" terrain="1,,1,1"/>
+ <tile id="576" terrain=",1,1,1"/>
+ <tile id="577" terrain=",1,,1"/>
+ <tile id="578" terrain="1,1,1,1"/>
+ <tile id="579" terrain="1,,1,"/>
+ <tile id="616">
+  <objectgroup draworder="index">
+   <object id="0" x="2" y="16">
+    <polygon points="0,0 14,-16 14,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="617">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0">
+    <polygon points="0,0 0,16 16,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="634" terrain=",1,,"/>
+ <tile id="635" terrain="1,1,,"/>
+ <tile id="636" terrain="1,,,"/>
+ <tile id="640">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="673">
+  <objectgroup draworder="index">
+   <object id="0" x="2" y="0" width="14" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="674">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ MTE100/Assets/Scripts/SFXManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SFXManager : MonoBehaviour {
+
+	public AudioSource playerHurt;
+	public AudioSource playerDead;
+	public AudioSource playerAttack;
+
+	private static bool sfxmanExists;
+
+	// Use this for initialization
+	void Start() {
+		if(!sfxmanExists)
+		{
+			sfxmanExists = true;
+			DontDestroyOnLoad(transform.gameObject);
+		} else {
+			Destroy (gameObject);
+		}
+	}
+}
